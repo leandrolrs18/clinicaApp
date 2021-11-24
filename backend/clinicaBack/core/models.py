1fa6cc9315174a9b32fce440502654e6b516d086
jpfcabral <jpfcabral@gmail.com>
2021-11-24 18:11:35 -0300

    add:paciente-model

--- backend/clinicaBack/core/models.py
+++ backend/clinicaBack/core/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Paciente(models.Model):
+    nome = models.CharField(max_length=50)
+    telefone = models.CharField(max_length=12)
+    email = models.CharField(max_length=50)
+    cpf = models.CharField(max_length=50)
+    senha = models.CharField(max_length=20) 
